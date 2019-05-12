89d6947d645e817dc5ceff543e90ab65bd80bbd0
Balogh Dénes <balogh.denes@yahoo.com>
2019-05-12 03:08:39 +0300

    Initial commit!

---
+++ LabII/Controllers/ExpensesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabII.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabII.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private ExpensesDbContext context;
+        public ExpensesController(ExpensesDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Expenses
+        [HttpGet]
+        public IEnumerable<Expense> Get()
+        {
+            return context.Expenses;
+        }
+        //// GET: api/Expenses
+        //[HttpGet]
+        //public IEnumerable<Expense> Get([FromQuery]DateTime? from, [FromQuery]DateTime? to)
+        //{
+        //    IQueryable<Expense> result = context.Expenses;
+        //    if(from == null && to == null)
+        //    {
+        //        return result;
+        //    }
+        //    if(from != null)
+        //    {
+        //        result = result.Expenses.Where(e => e.Date >= from);
+        //    }
+        //    if(to != null)
+        //    {
+        //        result = result.Expenses.Where(e => e.Date <= to);
+        //    }
+        //    return result;
+        //}
+
+        // GET: api/Expenses/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var existing = context.Expenses.FirstOrDefault(expense => expense.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(existing);
+        }
+
+
+        // POST: api/Expenses
+        [HttpPost]
+        public void Post([FromBody] Expense expense)
+        {
+            context.Expenses.Add(expense);
+            context.SaveChanges();
+        }
+
+
+
+        // PUT: api/Expenses/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Expense expense)
+        {
+            var existing = context.Expenses.AsNoTracking().FirstOrDefault(e => e.Id == id);
+            if (existing == null)
+            {
+                context.Expenses.Add(expense);
+                context.SaveChanges();
+                return Ok(expense);
+
+            }
+
+            expense.Id = id;
+            context.Expenses.Update(expense);
+            context.SaveChanges();
+            return Ok(expense);
+        }
+
+
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existing = context.Expenses.FirstOrDefault(expense => expense.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+            context.Expenses.Remove(existing);
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ LabII/Models/ExpensesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabII.Models
+{
+    public class ExpensesDbContext : DbContext
+    {
+        public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
