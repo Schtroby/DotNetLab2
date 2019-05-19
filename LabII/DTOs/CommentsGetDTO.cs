4f00f60520dc099f41d131bab54b84b293a62ec8
Balogh Dénes <balogh.denes@yahoo.com>
2019-05-19 11:12:22 +0300

    The last commit on lab3

--- LabII/DTOs/CommentsGetDTO.cs
+++ LabII/DTOs/CommentsGetDTO.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LabII.Models
+namespace LabII.DTOs
 {
-    public class Comment
+    public class CommentsGetDTO
     {
         public int Id { get; set; }
         public string Text { get; set; }
         public bool Important { get; set; }
-        
+        public int ExpenseId { get; set; }
     }
 }
